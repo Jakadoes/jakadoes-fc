3e3c4f393a86c918b7a6fc77851a5a9c0be4c0ea
Damien <damiensabljak@live.ca>
2021-02-28 16:37:27 -0500

    further development of control system features

--- fc/Core/Src/control.c
+++ fc/Core/Src/control.c
@@ -9,12 +9,11 @@
 //implement FIFO for previous results storing
 //Implement moving average to filter accelerometer data
 #include "mpu.h"
-uint16_t control_targets[3]; //holds target angles of [x,y,z] orientations
-uint16_t control_errors_last[3];  //holds errors of [x,y,z]
-uint16_t control_errors[3];  //holds errors of [x,y,z]
-uint16_t control_output[3];  //holds most recent output values of [x,y,z] orientations
-uint16_t control_k=1;
-uint16_t control_dk=1;
+int16_t control_targets[3]; //holds target angles of [x,y,z] orientations
+int16_t control_errors[6];  //holds errors of [x,y,z,xgyro,ygyro,zgyro]
+int16_t control_output[3];  //holds most recent output values of [x,y,z] orientations
+int16_t control_k=1;
+int16_t control_dk=0;
 uint32_t control_time_old = HAL_GetTick();
 uint32_t control_dt = 0;
 
@@ -26,26 +25,29 @@ void Control_Set_Target(x_tilt, y_tilt, z_tilt)
 }
 
 
-uint16_t Control_Calculate_Error(target, current)
+int16_t Control_Calculate_Error(target, current)
 {
-	uint8_t error = x_target - x_current;
+	int16_t error = x_target - x_current;
 	return error;
 }
 
 void Control_Update_Errors()
 {
 	Mpu_Update_Values();
-	control_errors_last[MPU_AXIS_X] = control_errors[MPU_AXIS_X];//store last results
-	control_errors_last[MPU_AXIS_Y] = control_errors[MPU_AXIS_Y];
-	control_errors_last[MPU_AXIS_Z] = control_errors[MPU_AXIS_Z];
-	control_errors[MPU_AXIS_X] = Control_Calculate_Error(control_targets[MPU_AXIS_X], mpu_acc[MPU_AXIS_X]);//update current results
+	//accelerometer
+	control_errors[MPU_AXIS_X] = Control_Calculate_Error(control_targets[MPU_AXIS_X], mpu_acc[MPU_AXIS_X]);
 	control_errors[MPU_AXIS_Y] = Control_Calculate_Error(control_targets[MPU_AXIS_Y], mpu_acc[MPU_AXIS_Y]);
 	control_errors[MPU_AXIS_Z] = Control_Calculate_Error(control_targets[MPU_AXIS_Z], mpu_acc[MPU_AXIS_Z]);
+	//gyroscope NOTE: switch rotation axis with accelerometer axis
+	//+'ve gyro error is in proper direction
+	control_errors[MPU_AXIS_Y+3] = ((control_errors[MPU_AXIS_X] > 0) - (control_errors[MPU_AXIS_X] < 0))* mpu_gyro[MPU_AXIS_X];
+	control_errors[MPU_AXIS_X+3] = ((control_errors[MPU_AXIS_Y] > 0) - (control_errors[MPU_AXIS_Y] < 0))* mpu_gyro[MPU_AXIS_Y];
+	control_errors[MPU_AXIS_Z+3] = ((control_errors[MPU_AXIS_Z] > 0) - (control_errors[MPU_AXIS_Z] < 0))* mpu_gyro[MPU_AXIS_Z];
 }
 
-uint16_t Control_Calculate_Output(const axis)
+int16_t Control_Calculate_Output(const axis)
 {	//fix dk control by adding history of error
-	output = control_k*(control_erros[axis]) + control_dk*((control_errors[axis] - control_errors_last[axis])/control_dt);
+	int16_t output = control_k*(control_errors[axis]) + -1*control_dk*(control_errors[axis + 3]);
 	return output;
 }
 
